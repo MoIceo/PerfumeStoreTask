823d9e71c562bcd66d34a3e53f585c49b4189c51
MoIceo <andrew.gorelkin@gmail.com>
2024-12-19 14:31:09 +0300

    Added API

--- WebApi/obj/Debug/net8.0-windows/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net8.0-windows/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7abd87bcbeab8b94486b8b504d8dbc84837d71dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
